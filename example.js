abf6caa72eb3ad1f95818fcecb7c6ddb2bee0346
Max Base <MaxBaseCode@Gmail.Com>
2020-12-02 10:46:25 +0330

    Adding more example in example.js

--- example.js
+++ example.js
@@ -13,5 +13,35 @@ database.run(`CREATE TABLE IF NOT EXISTS 'record'
     status                  INTEGER                      NOT NULL
 )`)
 
+// insert
+database.insert(`INSERT INTO record(meetingId, recordId, status) VALUES(?, ?, ?)`, [1000, 50505050, 1])
+database.insert(`INSERT INTO record(meetingId, recordId, status) VALUES(?, ?, ?)`, [5000, 45678050, 1])
+database.insert(`INSERT INTO record(meetingId, recordId, status) VALUES(?, ?, ?)`, [8000, 505, 0])
+database.insert(`INSERT INTO record(meetingId, recordId, status) VALUES(?, ?, ?)`, [9400, 564567, 0])
+database.insert(`INSERT INTO record(meetingId, recordId, status) VALUES(?, ?, ?)`, [3400, 9949494, 2])
+
+// selects
+database.selects(`SELECT * FROM record WHERE status = 0 ORDER BY ID ASC LIMIT 5`, [],
+(rows) => {
+  console.log(rows)
+  for(let row of rows) {
+    console.log(row)
+  }
+})
+
+// select
+database.select(`SELECT * FROM record WHERE meetingId = ? AND recordId = ?`, [5, 8], (res) => {
+  console.log(res)
+})
+
+// select COUNT
+database.select(`SELECT COUNT(*) as count FROM record WHERE meetingId = ? AND recordId = ?`, [5, 8], (res) => {
+  if(res[`count`]) {
+    console.log(res[`count`])
+  } else {
+    console.log(`Error: ${res}`)
+  }
+})
+
 // close
 database.close()
